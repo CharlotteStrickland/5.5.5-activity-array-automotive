09b934460f9f97820d739b60ba246e4a5170f959
Charlotte Strickland <charlottemarie81@gmail.com>
2022-06-01 13:19:08 -0400

    updated with information from activity on mercurySedan.js

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,54 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
+class Car extends VehicleModule {
+    constructor(make, model, year, mileage, color) {
+        super(make, model, year, mileage, color);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if(this.mileage > 30000) {
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            console.log("engine has started.");
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log(this.model + " " + this.make + " not ")
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+        }
+    }
+
+}
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
+
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
